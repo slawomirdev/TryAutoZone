35b6c3c9464785ec189e0b2f0341e9eda077965f
slawomirdev <mendykaslawomir@gmail.com>
2024-11-24 23:43:16 +0100

    Reservation trigger

---
+++ TryAutoZone/Data/Migrations/20241124221830_AddTriggerForAdditionalInformation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TryAutoZone.Data.Migrations
+{
+    public partial class AddTriggerForAdditionalInformation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+        CREATE TRIGGER SetDefaultAdditionalInformation
+        ON Reservations
+        AFTER INSERT
+        AS
+        BEGIN
+            UPDATE Reservations
+            SET AdditionalInformation = 'No additional information provided'
+            WHERE AdditionalInformation IS NULL OR AdditionalInformation = ''
+            AND Id IN (SELECT Id FROM INSERTED);
+        END;
+    ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP TRIGGER IF EXISTS SetDefaultAdditionalInformation;");
+        }
+    }
+}
